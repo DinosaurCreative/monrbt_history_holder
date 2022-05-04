fe3138791e4eef91b3c0e24a871ee92ceebff076
German <77802106+DinosaurCreative@users.noreply.github.com>
2022-05-05 02:18:30 +0300

    Update remindersHistory.js

--- controllers/remindersHistory.js
+++ controllers/remindersHistory.js
@@ -2,7 +2,7 @@ const RemindersHistory = require('../models/remindersHistory');
 
 module.exports.getHistory = (req, res) => {
   RemindersHistory.find({reminderId: 123})
-    .then(history => res.send({req}))
+    .then(history => res.send({req.params}))
     .catch(() => res.status(500).send('Ошибка на серваке'))
 }
 
