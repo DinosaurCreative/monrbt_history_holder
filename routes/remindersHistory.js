2a65eb123122d72fb4f99a6bd47e2a351e23b48e
German <77802106+DinosaurCreative@users.noreply.github.com>
2022-05-05 17:52:23 +0300

    Update remindersHistory.js

--- routes/remindersHistory.js
+++ routes/remindersHistory.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { getHistory, postAction } = require('../controllers/remindersHistory')
 
-router.post('/', postAction);
+router.post('/action', postAction);
 router.get('/', getHistory);
 
 module.exports = router;
