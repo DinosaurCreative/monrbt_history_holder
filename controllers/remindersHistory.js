882fb2a889c748f7d185a0132406c636b5c49b14
German <77802106+DinosaurCreative@users.noreply.github.com>
2022-05-05 20:55:49 +0300

    Update remindersHistory.js

--- controllers/remindersHistory.js
+++ controllers/remindersHistory.js
@@ -7,12 +7,16 @@ module.exports.getHistory = (req, res) => {
 }
 
 module.exports.postAction = (req, res) => {
-//"params", "query" ,"res", "body", "_body"
-  const response = Object.keys(req);
   RemindersHistory.create({
     actionType: req.body.params.actionType,
     reminderId: req.body.params.reminderId,
   })
+  .then((msg) => res.status(200).send('Добавлено'))
+  .catch(() => res.status(5000).send('Ошибка при добавления истории экшона'))
+}
+
+module.exports.postActions = (req, res) => {
+  RemindersHistory.create(req.body.params)
   .then((msg) => res.status(200).send('Добавлено'))
   .catch(() => res.status(5000).send('Ошибка при добавления истории экшона'))
 }
