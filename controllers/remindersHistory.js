651e0a8285b72fef33ffe96a96b3b7137835f7fd
German <gimalaevgerman@yandex.ru>
2022-05-10 02:43:56 +0300

    add create moment endpoint

--- controllers/remindersHistory.js
+++ controllers/remindersHistory.js
@@ -7,19 +7,40 @@ module.exports.getHistory = (req, res) => {
 }
 
 module.exports.postAction = (req, res) => {
-  RemindersHistory.create({
-    actionType: req.body.params.actionType,
-    reminderId: req.body.params.reminderId,
-    addedAtDay: req.body.params.addedAtDay,
-    addedAtTime: req.body.params.addedAtTime,
-    currentChatName: req.body.params.currentChatName || ' ',
-    previousChatName: req.body.params.previousChatName || ' ',
-  })
-  .then((msg) => res.status(200).send('Добавлено'))
-  .catch(() => res.status(500).send('Ошибка при добавления истории экшона'))
+  RemindersHistory.find({reminderId: req.query.params.reminderId})
+    .then(res => {
+      if(res.length < 1 ) {
+        RemindersHistory.create([{
+          actionType: 'Напоминание создано',
+          addedAtDay: req.body.params.addedAtDay,
+          addedAtTime: req.body.params.addedAtTime,
+        },
+        {
+          actionType: req.body.params.actionType,
+          reminderId: req.body.params.reminderId,
+          addedAtDay: req.body.params.addedAtDay,
+          addedAtTime: req.body.params.addedAtTime,
+          currentChatName: req.body.params.currentChatName || ' ',
+          previousChatName: req.body.params.previousChatName || ' ',
+        }
+      ]) 
+        return 
+      }
+      RemindersHistory.create({
+        actionType: req.body.params.actionType,
+        reminderId: req.body.params.reminderId,
+        addedAtDay: req.body.params.addedAtDay,
+        addedAtTime: req.body.params.addedAtTime,
+        currentChatName: req.body.params.currentChatName || ' ',
+        previousChatName: req.body.params.previousChatName || ' ',
+      })
+    })
+      .then((msg) => res.status(200).send('Добавлено'))
+      .catch(() => res.status(500).send('Ошибка при добавления истории экшона'))
 }
 
 module.exports.postActions = (req, res) => {
+  console.log(req.body.params[0])
   RemindersHistory.create(req.body.params)
   .then((msg) => res.status(200).send('Добавлено'))
   .catch(() => res.status(5000).send('Ошибка при добавления истории экшона'))
