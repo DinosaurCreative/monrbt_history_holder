dc06cf65ccbb2a5bd3cf06c2e582d5d52df5e315
German <gimalaevgerman@yandex.ru>
2022-05-09 23:04:35 +0300

    откатываю последний коммит

--- controllers/remindersHistory.js
+++ controllers/remindersHistory.js
@@ -7,14 +7,13 @@ module.exports.getHistory = (req, res) => {
 }
 
 module.exports.postAction = (req, res) => {
-  console.log(req.body)
   RemindersHistory.create({
     actionType: req.body.params.actionType,
     reminderId: req.body.params.reminderId,
     addedAtDay: req.body.params.addedAtDay,
     addedAtTime: req.body.params.addedAtTime,
-    currentChatName: req.body.params.currentChatName || ' ',
-    previousChatName: req.body.params.previousChatName || ' ',
+    // currentChatName: req.body.params.currentChatName || ' ',
+    // previousChatName: req.body.params.previousChatName || ' ',
   })
   .then((msg) => res.status(200).send('Добавлено'))
   .catch(() => res.status(500).send('Ошибка при добавления истории экшона'))
